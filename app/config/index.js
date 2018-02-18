22a51afd4c9809046953b7d562dd5762b3c3dc5a
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-02-18 19:34:11 +0100

    minor typo

--- app/config/index.js
+++ app/config/index.js
@@ -4,8 +4,8 @@ const port = 1337;
 
 module.exports = function (app) {
   app.use(express.static(path.join(__dirname, '../public/')));
-  app.use('/vue', express.static(path.join(__dirname, '/../node_modules/vue/dist/')));
-  app.use('/vue-router', express.static(path.join(__dirname, '/../node_modules/vue-router/dist/')));
+  app.use('/vue', express.static(path.join(__dirname, '../node_modules/vue/dist/')));
+  app.use('/vue-router', express.static(path.join(__dirname, '../node_modules/vue-router/dist/')));
 
   app.set('port', (process.env.PORT || port));
 };
