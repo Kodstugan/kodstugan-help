a161103087ebc9b08404eb248c4dca3d5b413b42
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-02-18 21:41:53 +0100

    fixed socket.io 404

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 const http = require('http').Server(app);
-const io = require('socket.io')(app.http);
+const io = require('socket.io')(http);
 const path = require('path');
 
 require('./routes/index.js')(app, path);
