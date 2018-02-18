c3f339db5abdbceaa80c84e43ac1a42d0f447c4b
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-02-18 21:45:04 +0100

    added init routes

--- app/public/assets/js/app.router.js
+++ app/public/assets/js/app.router.js
@@ -1,3 +1,23 @@
 "use strict";
 
-const router = new VueRouter({});
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      components: {
+        left_column: null,
+        right_column: null,
+        full_column: null,
+      }
+    },
+    {
+      path: '/new',
+      components: {
+        left_column: null,
+        right_column: null,
+        full_column: null,
+      }
+    }
+  ]
+});
