65fdcf0cba7a62c255ec9d4f0babbf18b668edad
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-02-18 23:34:19 +0100

    more test data and question-v css

--- app/public/assets/js/app.data.js
+++ app/public/assets/js/app.data.js
@@ -7,6 +7,12 @@ const data = {
       first_name: 'Fredrik',
       last_name: 'Andersson',
       message: 'Någon som är duktig på svansrekursion?'
+    },
+    1: {
+      image_url: 'https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/16406808_10155071389959073_5354531751417945894_n.jpg?oh=9e872c877f2884f5a3c403268c7e0c60&oe=5B149D77',
+      first_name: 'Fredrik',
+      last_name: 'Andersson',
+      message: 'Någon som är duktig på svansrekursion?'
     }
   }
 };
