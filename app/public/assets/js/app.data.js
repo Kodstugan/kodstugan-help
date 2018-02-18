45c5ab86aa7e0d9de56039b7ee17ac39ba3432b2
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-02-18 23:48:12 +0100

    question-v css, added more test data

--- app/public/assets/js/app.data.js
+++ app/public/assets/js/app.data.js
@@ -9,10 +9,22 @@ const data = {
       message: 'Någon som är duktig på svansrekursion?'
     },
     1: {
-      image_url: 'https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/16406808_10155071389959073_5354531751417945894_n.jpg?oh=9e872c877f2884f5a3c403268c7e0c60&oe=5B149D77',
-      first_name: 'Fredrik',
-      last_name: 'Andersson',
-      message: 'Någon som är duktig på svansrekursion?'
+      image_url: 'https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/10906113_10200182730008418_9158589838096036680_n.jpg?oh=6ac941d0fc15aa0dae0161323229521d&oe=5B1CF5CB',
+      first_name: 'Jesper',
+      last_name: 'Saxer',
+      message: 'Emacs krånglar hela tiden, någon som kan hjälpa mig fixa min .emacs fil?'
+    },
+    2: {
+      image_url: 'https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/19149204_1762801157070347_3552905809175526005_n.jpg?oh=deb48639442a5776deebf1c367f45088&oe=5B1C8575',
+      first_name: 'Kevin',
+      last_name: 'Alemi',
+      message: 'Vuejs klagar på undefined, förstår inte riktigt varför.'
+    },
+    3: {
+      image_url: 'https://scontent-arn2-1.xx.fbcdn.net/v/t31.0-8/14257489_10207662234296743_5548954200368099881_o.jpg?oh=2276a54839395fd58a611c9a6a05c53d&oe=5B1E7D66',
+      first_name: 'Sebastian',
+      last_name: 'Lhådö',
+      message: 'Finns det någon som är duktig på gcc här?'
     }
   }
 };
