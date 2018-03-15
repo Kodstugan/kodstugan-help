e55a051cf1f6217b2821444a2d5f44224b4230c3
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-03-15 17:28:09 +0100

    minor formatting change

--- app/socket/index.js
+++ app/socket/index.js
@@ -4,9 +4,13 @@ module.exports = function (app, io) {
   io.on('connection', function (socket) {
     socket.emit('client/update-info', {id: socket.request.user.id, name: socket.request.user.displayName});
 
-    socket.on('client/question-add', function (message) {});
-    socket.on('client/question-del', function (message) {});
-    socket.on('client/question-comment-add', function (message) {});
-    socket.on('client/question-comment-del', function (message) {});
+    socket.on('client/question-add', function (message) {
+    });
+    socket.on('client/question-del', function (message) {
+    });
+    socket.on('client/question-comment-add', function (message) {
+    });
+    socket.on('client/question-comment-del', function (message) {
+    });
   });
 };
