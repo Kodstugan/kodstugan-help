313429a570ea3d5469db0059d24cca84bc9fc399
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-03-28 15:00:35 +0200

    mark as solved is now working

--- app/socket/index.js
+++ app/socket/index.js
@@ -61,6 +61,11 @@ module.exports = function (app, io) {
         cooldowns.splice(index, 1);
         socket.emit('client/onCooldownRemove');
       }, 10 * 1000);
+    });
+
+    socket.on('client/questionSolved', function (data) {
+      questions[data.key].solved = true;
+      io.emit('client/onQuestionAdd', {key: data.key, question: questions[data.key]});
     })
   });
 
